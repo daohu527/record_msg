c097f121e072a6523ff415e64d5738853f11caa8
daohu527 <daohu527@gmail.com>
2022-11-23 18:10:47 +0800

    feat: update version to 0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="record_msg",
-    version="0.0.8",
+    version="0.0.9",
     author="daohu527",
     author_email="daohu527@gmail.com",
     description="Record message parse helper function",
